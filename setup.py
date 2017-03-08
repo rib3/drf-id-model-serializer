32c3e01ba2c7d09f0ce9d99feb72b2cb5dc9c45f
Bob Black <git@riblack.com>
2017-03-08 16:59:31 -0600

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='drf-relation-id',
+      version='1.0',
+      description='Add _id when seralizing relationships w/ DRF',
+      url='https://github.com/rib3/drf-relation-id',
+      author='Bob Black',
+      author_email='git@riblack.com',
+      license='MIT',
+      packages=['drf_relation_id'],
+      zip_safe=False)
